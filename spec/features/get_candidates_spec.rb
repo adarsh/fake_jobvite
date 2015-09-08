e34033b33fefa28041f5a8f46f59d9f86807f2df
Adarsh Pandit <adarshpandit@gmail.com>
2015-09-08 14:24:21 -0700

    add a second spec to check the xml response

--- spec/features/get_candidates_spec.rb
+++ spec/features/get_candidates_spec.rb
@@ -7,10 +7,17 @@ RSpec.describe 'FakeJobvite' do
     expect(response.code).to eq(200)
   end
 
-  it 'sends back XML data' do
+  it 'sends back the fixture file' do
     response = RestClient.get('https://api.jobvite.com/candidates')
     file_results = File.open('fixtures/ResumeExample.xml', 'r').read
 
     expect(response.body).to eq file_results
+  end
+
+  it 'sends back XML data' do
+    response = RestClient.get('https://api.jobvite.com/candidates')
+    content_type = response.headers.fetch(:content_type)
+
+    expect(content_type).to include 'text/xml'
   end
 end
