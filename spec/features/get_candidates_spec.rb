8aeccc3de88c4f1b88b3cfcfa0881e473a429242
Adarsh Pandit <adarshpandit@gmail.com>
2015-09-08 14:18:18 -0700

    green when returning results

--- spec/features/get_candidates_spec.rb
+++ spec/features/get_candidates_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 RSpec.describe 'FakeJobvite' do
-  it 'returns the candidate data' do
+  it 'responds with a 200' do
     response = RestClient.get('https://api.jobvite.com/candidates')
 
     expect(response.code).to eq(200)
-    expect(response.body).to eq 'hiya'
+  end
+
+  it 'sends back XML data' do
+    response = RestClient.get('https://api.jobvite.com/candidates')
+    file_results = File.open('fixtures/ResumeExample.xml', 'r').read
+
+    expect(response.body).to eq file_results
   end
 end
