626ec42944f9366ac2c47233d5c74cd890de1362
Adarsh Pandit <adarshpandit@gmail.com>
2015-09-08 14:09:32 -0700

    return a fixture file

--- lib/fake_jobvite.rb
+++ lib/fake_jobvite.rb
@@ -2,7 +2,14 @@ require 'sinatra/base'
 
 class FakeJobvite < Sinatra::Base
   get '/candidates' do
-    body 'hiya'
+    content_type 'text/xml'
+    body fixture_file('ResumeExample.xml')
     status 200
+  end
+
+  private
+
+  def fixture_file(filename)
+    File.open("fixtures/#{filename}", 'r').read
   end
 end
